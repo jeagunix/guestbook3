5178c3122b177d15e221a647c8c60a39229aa634
JeaGunix <dlwprn1541@gmail.com>
2019-09-30 19:19:26 +0900

    pom.xml 수정

--- src/main/java/kr/co/itcen/guestbook/dao/GuestBookDao.java
+++ src/main/java/kr/co/itcen/guestbook/dao/GuestBookDao.java
@@ -18,7 +18,7 @@ public class GuestBookDao {
 		Connection connection = null;
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");// 2. 연결하기
-			String url = "jdbc:mariadb://192.168.43.192:3306/webdb?characterEncoding=utf8";
+			String url = "jdbc:mariadb://192.168.1.43:3306/webdb?characterEncoding=utf8";
 			connection = DriverManager.getConnection(url, "webdb", "webdb");
 		} catch (ClassNotFoundException e) {
 			System.out.println("Fail to Loading Driver: " + e);
